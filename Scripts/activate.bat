4b3821e650dfde1bc792c69fec03c8e9de357938
a910230 <a910230@gmail.com>
2020-03-01 17:28:57 -0600

    ignore *.pyc

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\python\crawler
+set VIRTUAL_ENV=c:\python\crawler
 
 if not defined PROMPT set PROMPT=$P$G
 
